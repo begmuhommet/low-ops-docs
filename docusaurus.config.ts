23ac1a9a454b76f87aac772496e3d7dea621d63b
bega <begmuhommet.jumayev@gmail.com>
2025-04-02 13:18:44 +0800

    fix bugs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,42 +1,42 @@
-import * as Preset from '@docusaurus/preset-classic';
-import type { Config } from '@docusaurus/types';
-import { themes as prismThemes } from 'prism-react-renderer';
+import * as Preset from "@docusaurus/preset-classic";
+import type { Config } from "@docusaurus/types";
+import { themes as prismThemes } from "prism-react-renderer";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'Low-Ops',
-  tagline: 'Private Cloud Infrastructure',
-  favicon: 'img/favicon.ico',
+  title: "Low-Ops",
+  tagline: "Private Cloud Infrastructure",
+  favicon: "img/favicon.ico",
   // Set the production url of your site here
-  url: 'https://low-ops.com',
+  url: "https://low-ops.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'cinaq', // Usually your GitHub org/user name.
-  projectName: 'low-ops', // Usually your repo name.
+  organizationName: "cinaq", // Usually your GitHub org/user name.
+  projectName: "low-ops", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
           // routeBasePath: '/',
-          sidebarPath: './sidebars.ts',
+          sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
 
@@ -57,7 +57,7 @@ const config: Config = {
         //   onUntruncatedBlogPosts: 'warn',
         // },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -70,12 +70,12 @@ const config: Config = {
       },
     },
     // Replace with your project's social card
-    image: 'img/logo.svg',
+    image: "img/logo.svg",
     navbar: {
       logo: {
-        alt: 'Low-Ops Logo',
-        src: 'img/logo-full-dark.svg',
-        srcDark: 'img/logo-full-light.svg',
+        alt: "Low-Ops Logo",
+        src: "img/logo-full-dark.svg",
+        srcDark: "img/logo-full-light.svg",
       },
       items: [
         // { type: 'docSidebar', sidebarId: 'tutorialSidebar', position: 'left', label: 'Docs' },
@@ -84,22 +84,22 @@ const config: Config = {
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
-        { title: 'Docs', items: [{ label: 'Docs', to: '/docs/introduction' }] },
+        { title: "Docs", items: [{ label: "Docs", to: "/docs/introduction/overview" }] },
         {
-          title: 'Community',
+          title: "Community",
           items: [
-            { label: 'Stack Overflow', href: 'https://stackoverflow.com/questions/tagged/docusaurus' },
-            { label: 'Discord', href: 'https://discordapp.com/invite/docusaurus' },
-            { label: 'X', href: 'https://x.com/docusaurus' },
+            { label: "Stack Overflow", href: "https://stackoverflow.com/questions/tagged/docusaurus" },
+            { label: "Discord", href: "https://discordapp.com/invite/docusaurus" },
+            { label: "X", href: "https://x.com/docusaurus" },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
-            { label: 'Blog', to: '/blog' },
-            { label: 'GitHub', href: 'https://github.com/facebook/docusaurus' },
+            // { label: "Blog", to: "/blog" },
+            // { label: "GitHub", href: "https://github.com/facebook/docusaurus" },
           ],
         },
       ],

0af287a42c0eaecb11b0c9dae6e5c2e6c2392f35
bega <begmuhommet.jumayev@gmail.com>
2025-04-04 12:10:22 +0800

    environment docs

---
+++ src/components/ReDeployLink.tsx
@@ -0,0 +1,9 @@
+const ReDeployLink = () => {
+  return (
+    <a href="/docs/environment/deployment" className="redeploy-link">
+      Deploy Environment.
+    </a>
+  );
+};
+
+export default ReDeployLink;

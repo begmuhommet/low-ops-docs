49487e4c470f0d4b82add329720a63765b4dc92d
bega <begmuhommet.jumayev@gmail.com>
2025-04-04 13:05:56 +0800

    Deploy website - based on 77cd223c1b7b9c7b2075bc8386f5f8356e753ce1

--- src/components/ReDeployLink.tsx
+++
@@ -1,9 +0,0 @@
-const ReDeployLink = () => {
-  return (
-    <a href="/docs/environment/deployment" className="redeploy-link">
-      Deploy Environment.
-    </a>
-  );
-};
-
-export default ReDeployLink;
